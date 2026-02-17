47454fa57c93f7fbc59e60deeb596763852c4831
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2026-02-17 14:33:59 +0100

    Fixade kulornas  kod

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -12,6 +12,6 @@ public class BulletController : MonoBehaviour
 
   void Update()
   {
-    transform.Translate(transform.forward * speed * Time.deltaTime);
+    transform.Translate(Vector3.forward * speed * Time.deltaTime);
   }
 }
